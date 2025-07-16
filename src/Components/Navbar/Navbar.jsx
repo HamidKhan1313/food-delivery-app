4f732ec3e400fc6baf54dde028fe54d4ea060e6e
HamidKhan1313 <hamidkhantanol@gmail.com>
2025-07-16 12:44:53 +0500

    changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import { assets } from '../../assets/assets'
+import assets from '../../assets/assets.js';
 import './Navbar.css'
 function Navbar() {
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" />
+      <ul className='navbar-menu'>
+     <li>home</li>
+     <li>menue</li>
+     <li>mobile-app</li>
+     <li>contact-us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>sign in</button>
+      </div>
     </div>
   )
 }
 
 export default Navbar
+
