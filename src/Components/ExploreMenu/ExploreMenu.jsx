bd4811073dd0a31833b485f28c06b16819de85fc
HamidKhan1313 <hamidkhantanol@gmail.com>
2025-07-19 15:24:53 +0500

    changes

--- src/Components/ExploreMenu/ExploreMenu.jsx
+++ src/Components/ExploreMenu/ExploreMenu.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './ExploreMenu.css'
 import { menue_list } from '../../assets/assets'
-function ExploreMenu() {
+function ExploreMenu(category, setCategory) {
   return (
     <div className='explore-menu' id='explore-menu'>
         <h1> Explore over menue</h1>
@@ -9,7 +9,7 @@ function ExploreMenu() {
       <div className='explore-menu-list'>
         {menue_list.map((item, index) => {
             return(
-                <div  key={index}className='explore-menu-list-item'>
+                <div  onClick={() => setCategory(prev =>prev===item.menue_name?'All':item.menue_name)} key={index}className='explore-menu-list-item'>
                 <img src={item.menue_image} alt="" />
                 <p>{item.menue_name}</p>
                 </div>
@@ -17,6 +17,7 @@ function ExploreMenu() {
         })}
 
       </div>
+      <hr/>
     </div>
   )
 }
