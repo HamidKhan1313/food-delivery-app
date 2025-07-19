00572e5ee9345b1e1b7946b2b0a79b18f7f72792
HamidKhan1313 <hamidkhantanol@gmail.com>
2025-07-19 23:31:20 +0500

    changes

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -6,6 +6,7 @@ import bag_icon from './bag_icon.png'
 import cross_icon from './cross_icon.png'
 import facebook_icon from './facebook_icon.png';
 import header_img from './header_img.png'
+import rating_starts from './rating_starts.png'
 import menu_1 from './menu_1.png'
 import menu_2 from './menu_2.png'
 import menu_3 from './menu_3.png'
@@ -14,6 +15,21 @@ import menu_5 from './menu_5.png'
 import menu_6 from './menu_6.png'
 import menu_7 from './menu_7.png'
 import menu_8 from './menu_8.png'
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+
 
 
 
@@ -27,7 +43,8 @@ export default {
   bag_icon,
   cross_icon,
   facebook_icon,
-  header_img
+  header_img,
+  rating_starts
   
   
 };
@@ -65,6 +82,134 @@ export const menue_list = [
     menue_image: menu_8
   }
 
+]
+export const food_list = [
+{
+  _id: '1',
+  name: 'Greek salad',
+  image: food_1,
+  price: 12,
+  description: "Food provide essential nutrients for overall health and well-being",
+  category: 'Salad'
+
+},
+{
+  _id: '2',
+  name: 'Veg salad',
+  image: food_2,
+  price: 18,
+  description: "Food provide essential nutrients for overall health and well-being",
+  category: 'Salad'
+
+},
+{
+  _id: '3',
+  name: 'Clover salad',
+  image: food_3,
+  price: 16,
+  description: "Food provide essential nutrients for overall health and well-being",
+  category: 'Salad'
+
+},
+{
+  _id: '4',
+  name: 'Chicken salad',
+  image: food_4,
+  price: 24,
+  description: "Food provide essential nutrients for overall health and well-being",
+  category: 'Salad'
+
+},
+{
+  _id: '6',
+  name: 'Lasagna Rolls',
+  image: food_5,
+  price: 14,
+  description: "Food provide essential nutrients for overall health and well-being",
+  category: 'Rolls'
+
+},
+{
+  _id: '6',
+  name: 'Chicken Rolls',
+  image: food_6,
+  price: 20,
+  description: "Food provide essential nutrients for overall health and well-being",
+  category: 'Rolls'
+
+},
+{
+  _id: '7',
+  name:  'Veg Rolls',
+  image: food_7,
+  price: 17,
+  description: "Food provide essential nutrients for overall health and well-being",
+  category: 'Rolls'
+
+},
+{
+  _id: '8',
+  name: 'Ripple Ice Creem',
+  image: food_8,
+  price: 16,
+  description: "Food provide essential nutrients for overall health and well-being",
+  category: 'IceCream'
+
+},
+{
+  _id: '9',
+  name: 'Fruit Ice Creem',
+  image: food_9,
+  price: 21,
+  description: "Food provide essential nutrients for overall health and well-being",
+  category: 'IceCream'
+
+},
+{
+  _id: '10',
+  name: 'Jar Ice Creem',
+  image: food_10,
+  price: 10,
+  description: "Food provide essential nutrients for overall health and well-being",
+  category: 'IceCream'
+
+},
+{
+  _id: '11',
+  name: 'Vanilla Ice Creem',
+  image: food_11,
+  price: 12,
+  description: "Food provide essential nutrients for overall health and well-being",
+  category: 'IceCream'
+
+},
+{
+  _id: '12',
+  name: 'Chicken Sandwich',
+  image: food_12,
+  price: 12,
+  description: "Food provide essential nutrients for overall health and well-being",
+  category: 'Sandwich'
+
+},
+{
+  _id: '13',
+  name: 'Vegan Sandwich',
+  image: food_13,
+  price: 18,
+  description: "Food provide essential nutrients for overall health and well-being",
+  category: 'Sandwich'
+
+},
+{
+  _id: '14',
+  name: 'Grilled Sandwich',
+  image: food_14,
+  price: 16,
+  description: "Food provide essential nutrients for overall health and well-being",
+  category: 'Sandwich'
+
+}
 ]
   
 
